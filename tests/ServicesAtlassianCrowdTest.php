afdd6ef269ffaa0751fa065b37c5d8ceee728d75
Daniel Convissor <danielc@php.net>
2011-12-07 01:09:55 +0000

    Remove error reporting in tests. (Bug #19021)
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Atlassian_Crowd/trunk@320541 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/ServicesAtlassianCrowdTest.php
+++ tests/ServicesAtlassianCrowdTest.php
@@ -30,7 +30,6 @@
 
 //Remove the comment below if you want test from source
 set_include_path('../../..'.PATH_SEPARATOR.get_include_path());
-error_reporting(E_ALL);
 
 require_once 'PHPUnit/Framework.php';
 require_once 'Services/Atlassian/Crowd.php';
